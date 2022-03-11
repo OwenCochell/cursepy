f3a3093a3cb94db55c9a52980690c1fefa181182
Owen-Cochell <owencochell@gmail.com>
2022-03-11 16:48:48 -0500

    Removed testing files

--- test.py
+++
@@ -1,8 +0,0 @@
-from cursepy import CurseClient
-
-client = CurseClient()
-search = client.get_search()
-search.pageSize = 5
-search.searchFilter = "witchery"
-for i in client.search(432, client.ADDON_SEARCH, search):
-    print(i.name)
